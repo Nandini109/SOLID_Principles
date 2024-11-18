1e6b31e407559f000966d84cf523770980f0330e
Eni <nandiniparmar7781@gmail.com>
2024-11-18 13:20:12 +0530

    Player mesh and animation

--- SolidPrinciples_Assignment2/Assets/Scripts/Player/PlayerCharacter.cs
+++ SolidPrinciples_Assignment2/Assets/Scripts/Player/PlayerCharacter.cs
@@ -7,7 +7,7 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField] private float rotationSpeed = 10f; 
     private CharacterController characterController;
     private Transform cameraTransform;
-
+    private Animator animator;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -18,6 +18,7 @@ public class PlayerCharacter : MonoBehaviour
             Debug.LogError("CharacterController component missing!");
         }
 
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -33,6 +34,8 @@ public class PlayerCharacter : MonoBehaviour
 
         Vector3 inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
 
+        
+
         if (inputDirection.magnitude > 0.1f)
         {
             Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
@@ -44,6 +47,13 @@ public class PlayerCharacter : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
             characterController.Move(moveDirection * (moveSpeed * Time.deltaTime));
+
+            animator.SetBool("IsWalking", true);
+        }
+
+        if (inputDirection.magnitude < 0.1f)
+        {
+            animator.SetBool("IsWalking", false);
         }
     }
 
